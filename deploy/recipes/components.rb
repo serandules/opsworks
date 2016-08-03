5aae90be7915963980c91fb611e11e15292f0c01
Ruchira Wageesha <ruchira.wageesha@gmail.com>
2016-08-03 08:44:29 +0530

    updating

--- deploy/recipes/components.rb
+++ deploy/recipes/components.rb
@@ -7,49 +7,52 @@
 include_recipe 'nodejs'
 include_recipe 'component'
 
-app = search(:aws_opsworks_app).first
-app_path = "/srv/#{app['shortname']}"
-scripts_path = "/tmp/deploy"
+search(:aws_opsworks_app).each do |app|
+  deploy = app['environment']['DEPLOY']
+  next if deploy != 'true'
+  app_path = "/srv/#{app['shortname']}"
+  scripts_path = '/tmp/deploy'
 
-directory app_path do
-  owner 'root'
-  group 'root'
-  mode '0755'
-  action :create
-  recursive true
-end
+  directory app_path do
+    owner 'root'
+    group 'root'
+    mode '0755'
+    action :create
+    recursive true
+  end
 
-directory scripts_path do
-  owner 'root'
-  group 'root'
-  mode '0755'
-  action :create
-  recursive true
-end
+  directory scripts_path do
+    owner 'root'
+    group 'root'
+    mode '0755'
+    action :create
+    recursive true
+  end
 
-Chef::Log.info("setting up environment variables")
-template "bootstrap" do
-  source "bootstrap.erb"
-  path "#{app_path}/.bootstrap"
-  mode "0644"
-  owner "root"
-  group "root"
-  variables ({:environment => app['environment']})
-end
+  Chef::Log.info('setting up environment variables')
+  template 'bootstrap' do
+    source 'bootstrap.erb'
+    path '#{app_path}/.bootstrap'
+    mode '0644'
+    owner 'root'
+    group 'root'
+    variables ({:environment => app['environment']})
+  end
 
-Chef::Log.info("deploying components")
-cookbook_file "/tmp/deploy/components" do
-  source "components"
-  mode 0755
-end
+  Chef::Log.info('deploying components')
+  cookbook_file '/tmp/deploy/components' do
+    source 'components'
+    mode 0755
+  end
 
-cookbook_file "/tmp/deploy/components-uploader" do
-  source "components-uploader"
-  mode 0755
-end
+  cookbook_file '/tmp/deploy/components-uploader' do
+    source 'components-uploader'
+    mode 0755
+  end
 
-execute "components" do
-  user "root"
-  cwd "/tmp/deploy"
-  command "./components #{app['shortname']} #{app_path} #{app['app_source']['url']} #{app['app_source']['revision']}"
+  execute 'components' do
+    user 'root'
+    cwd '/tmp/deploy'
+    command "./components #{app['shortname']} #{app_path} #{app['app_source']['url']} #{app['app_source']['revision']}"
+  end
 end
