b66e1f255a55190fbe6cb9017e3ff5a5b7528422
Ruchira Wageesha <ruchira.wageesha@gmail.com>
2017-10-18 13:54:06 +0530

    updating

--- mongodb/attributes/mms_agent.rb
+++
@@ -1,2 +0,0 @@
-default['mongodb']['mms_agent']['api_key'] = ''
-default['mongodb']['mms_agent']['secret_key'] = ''
--- mongodb/recipes/configure.rb
+++ mongodb/recipes/configure.rb
@@ -8,6 +8,8 @@ search(:aws_opsworks_instance).each do |instance|
   status = instance['status']
   hostname = instance['hostname']
 
+  Chef::Log.info("configuring instance #{hostname} with status #{status}")
+
   cookbook_file "/tmp/mongodb/configure" do
     source "configure"
     mode 0755
